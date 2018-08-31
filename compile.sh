3d6e0a054ff5dc0b1cc7590ccad6cd0bcd3604b3
Sergio Torres <sdtorresl@unal.edu.co>
2018-08-30 23:16:26 -0500

    Script for execute compilation faster

---
+++ compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mvn clean compile test
+
+rm -rf spring-boot-generate/src/main/resources/migrations/
+rm -rf spring-scaffold-cli/src/main/java/br/com/example/
+rm -rf spring-scaffold-cli/src/main/resources/application.properties
+rm -rf spring-scaffold-cli/src/main/resources/templates/user/
+rm -rf spring-scaffold-cli/src/main/resources/templates/usermodel/
+rm -rf spring-scaffold-cli/src/main/java/com/
